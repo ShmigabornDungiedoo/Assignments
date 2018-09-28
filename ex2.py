e24e87fde1696d71436a4bddb2567e441ee08b20
ShmigabornDungiedoo <relonet95@gmail.com>
2018-09-28 16:15:38 +0100

    all the pythons

---
+++ ex2.py
@@ -0,0 +1,20 @@
+print("This are how many animals I have:")
+
+print("cows", 15+30/6)
+print("sheep", 100-30*3 % 4)
+
+print("Now I will count my plants:")
+
+print(3+2+1-5+4% 2-1/4+6)
+
+print("Is it true that 3+2<5-7?")
+print(3+2<5-7)
+
+print("what is 3+2?", 3+2)
+print("what is 5-7?", 5-7)
+
+print("Can you see why that printed False earlier?")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ ex4.py
@@ -0,0 +1,16 @@
+my_name = 'Yurii Skorobogatko'
+my_age = 19
+my_height = 183 #cm
+my_weight = 64 #kg
+my_eyes = 'brown'
+my_hair = 'brown'
+is_heavy = my_weight > 3000
+
+print(f"Let's talk about {my_name}.")
+print(f"He is {my_height} centimetres tall.")
+print(f"He's {my_weight} kg heavy.")
+print(f"It is {is_heavy} that he is overweight.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
