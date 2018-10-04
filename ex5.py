0434742e270c0e4c59f55f215f62d1234462cb91
ShmigabornDungiedoo <relonet95@gmail.com>
2018-10-04 11:17:17 +0100

    beer song

---
+++ ex5.py
@@ -0,0 +1,6 @@
+beer= 99
+while beer>0:
+    print(str(beer) +   " bottles of beer on the wall, " + str(beer) + " bottles of beer.")
+    beer = beer -1
+print("no more bottles of beer on the wall, no more bottles of beer.")
+print("Go to the store and buy some more, 99 bottles of beer on the wall...")
