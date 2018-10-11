80af978463c9d7b31ec0bc4b214970ffc9ca9759
ShmigabornDungiedoo <relonet95@gmail.com>
2018-10-11 11:44:36 +0100

    not finished yet

---
+++ ex9.py
@@ -0,0 +1,11 @@
+annual_sallary=float(input("What is your annual sallary?"))
+total_cost=1000000
+r=0.04
+semi_annual_raise=0.07
+down_payment=250000
+portion_deposit=total_cost*0.2
+current_savings=0
+portion_saved = (annual_sallary/12)*savings_percent
+
+
+print("Number of months: "+ str(time))
